741ad211db11670c85a92a4769d31d4ba7bcda76
Lucas Oliveira <lucaslou189@gmail.com>
2021-07-29 10:36:19 -0300

    Create integracao1.jsx

---
+++ exercicios/pages/css/integracao1.jsx
@@ -0,0 +1,11 @@
+// A importação de css será via _app.js
+export default function integracao() {
+// exemplo de css global
+    return (
+        <div className="integracao1">
+            <div className="vermelha">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <div className="branca">Texto #03</div>
+        </div>
+    )
+}
