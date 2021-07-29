d46b09f7e036ee2ccf3d774b09a52fc9315a2184
Lucas Oliveira <lucaslou189@gmail.com>
2021-07-29 10:36:07 -0300

    Create fragment1.jsx

---
+++ exercicios/pages/basicos/fragment1.jsx
@@ -0,0 +1,10 @@
+import React from 'react' //para usar o React.Fragment
+export default function fragment() {
+    return (
+        <React.Fragment>
+            <h1>Título</h1>
+            <h2>Subtitulo</h2>
+        </React.Fragment>
+
+    )
+}
