71bfd8db88cc26087b2adc3037d84f7a061382f6
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-03 15:47:01 -0300

    Update condicional2.jsx

--- exercicios/pages/render/condicional2.jsx
+++ exercicios/pages/render/condicional2.jsx
@@ -1,7 +1,12 @@
+import If from "../../components/if";
+
 export default function condicional2(){
+    const numero = 3
     return(
         <div>
-            
+            <If teste={numero % 2 === 0}>
+                <h1>O número é par</h1>
+            </If>
         </div>
     )
 }
