9293119f77f52c3fedbbdfe008a2b58e92cbf862
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-03 09:20:34 -0300

    Create Linha.jsx

---
+++ tabuleiro/components/Linha.jsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Linha.module.css'
+import Subdivisao from './Subdivisao'
+export default function Linha(props){
+    return(
+        <div className={styles.linha}>
+            <Subdivisao preta={props.preta}/>
+            <Subdivisao preta={!props.preta}/>
+            <Subdivisao preta={props.preta}/>
+            <Subdivisao preta={!props.preta}/>
+            <Subdivisao preta={props.preta}/>
+            <Subdivisao preta={!props.preta}/>
+            <Subdivisao preta={props.preta}/>
+            <Subdivisao preta={!props.preta}/>
+        </div>
+    )
+}
