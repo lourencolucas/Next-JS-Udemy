2a2b2793d9a4a4b3a4a4aa94248684bd0dbd1f86
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-03 16:39:55 -0300

    Update direta.jsx

--- exercicios/pages/comunicacao/direta.jsx
+++ exercicios/pages/comunicacao/direta.jsx
@@ -1,7 +1,10 @@
+import Pai from "../../components/direta/Pai";
+
 export default function direta(){
     return (
         <div>
-            <h1>Direta</h1>
+            <h1>Comunicação Direta</h1>
+            <Pai familia="Albuquerque" nome="Padrão"/>
         </div>
     )
 }
