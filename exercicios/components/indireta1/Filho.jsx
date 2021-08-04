37cb7bea54056fbc745091deaf4e55f2226ece57
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-04 10:47:45 -0300

    Comunicação indireta

--- exercicios/components/indireta1/Filho.jsx
+++ exercicios/components/indireta1/Filho.jsx
@@ -1,7 +1,10 @@
 export default function Filho(props) {
+    // console.log(props.funcao)
     return(
         <div>
-            
+           <h1>Filho</h1> 
+           <button onClick={props.funcao}>Falar com o componente pai #01</button>
+           <button onClick={() => props.funcao("Passei no ENEM")}>Falar com o componente pai #02</button>
         </div>
     )
 }
--- exercicios/components/indireta1/Pai.jsx
+++ exercicios/components/indireta1/Pai.jsx
@@ -1,7 +1,13 @@
+import Filho from "./Filho";
+
 export default function Pai(props) {
+    function falarComigo(param){
+        console.log(param)
+        // console.log("Alguém Falou Comigo")
+    }
     return(
         <div>
-            
+            <Filho funcao={falarComigo}/>
         </div>
     )
 }
