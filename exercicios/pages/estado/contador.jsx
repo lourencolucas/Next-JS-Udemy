26a02cff4d4186e3ae0d557b570c4d4b8cabcd3b
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-05 08:33:38 -0300

    Create contador.jsx

---
+++ exercicios/pages/estado/contador.jsx
@@ -0,0 +1,12 @@
+export default function contador() {
+    return (
+        <div>
+            <h1>Contador </h1>
+            <div>
+                <span>Valor: ???</span>
+            </div>
+            <button>+1</button>
+            <button>-1</button>
+        </div>
+    )
+}
