8797d109616ec89f17fb8e9af4698658793fd109
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-05 11:18:49 -0300

    Create Contador.jsx

---
+++ exercicios/components/Contador.jsx
@@ -0,0 +1,12 @@
+//49. Componente Baseado em Classe
+import { Component } from "react";
+
+export default class Contador extends Component{
+    render(){ 
+        return(
+            <div>
+                <h1>Contador (usando Classe)</h1>
+            </div>
+        )
+    }
+}
