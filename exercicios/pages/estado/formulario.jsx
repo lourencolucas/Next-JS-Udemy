9e9d9837c63f973ab28c9fd528f83e2c631aa4e0
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-05 10:41:13 -0300

    Update formulario.jsx

--- exercicios/pages/estado/formulario.jsx
+++ exercicios/pages/estado/formulario.jsx
@@ -1,10 +1,23 @@
+//44 - COMPONENTES CONTROLADOS 
 import { useState } from "react"
 
 export default function formulario() {
     const [valor, setValor] = useState("Inicial")
-    return(
-        <div>
-            <input type="text" value={valor}/>
+
+    function alterarInput() {
+        setValor(valor + "!")
+    }
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column"
+        }}>
+            <span>{valor}</span>
+            <input type="text" value={valor}
+                onChange={e => setValor(e.target.value)}
+            />
+            <button onClick={alterarInput}>Alterar</button>
         </div>
     )
 }
