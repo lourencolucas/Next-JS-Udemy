5b87abde777b348f6bbcb571a79721fac13d1791
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-05 09:10:27 -0300

    Update contador.jsx

--- exercicios/pages/estado/contador.jsx
+++ exercicios/pages/estado/contador.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react"
+import ContadorDisplay from "../../components/ContadorDisplay"
+
 export default function contador() {
+    const [numero, setNumero] = useState(0)
+    //arrow function
+    const inc = () => setNumero(numero + 1)
+    const dec = () => setNumero(numero - 1)
+    //Estilos
+    const estilo = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    }
     return (
-        <div>
+        <div style={estilo}>
             <h1>Contador </h1>
             <div>
-                <span>Valor: ???</span>
+                <h2>Usando Estado Interno</h2>
+                <span>Valor: {numero}</span>
             </div>
-            <button>+1</button>
-            <button>-1</button>
+            <div>
+                <h2>Usando Componente</h2>
+                <ContadorDisplay numero={numero} />
+            </div>
+            <div>
+                <button onClick={inc}>+1</button>
+                <button onClick={dec}>-1</button>
+            </div>
         </div>
     )
 }
