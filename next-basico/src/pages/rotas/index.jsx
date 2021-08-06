37013701fda4ad9a790cedb5aa110dc74f3d7cec
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-06 11:22:32 -0300

    Update index.jsx

--- next-basico/src/pages/rotas/index.jsx
+++ next-basico/src/pages/rotas/index.jsx
@@ -1,7 +1,20 @@
+import Link from 'next/link'
+
 export default function rotas() {
     return (
         <div>
             <h1>Rotas index</h1>
+            <ul>
+                <Link href="/rotas/params?nome=leonardo&id=3">
+                    <li>Params</li>
+                </Link>
+                <Link href="/rotas/123/buscar">
+                    <li>Buscar</li>
+                </Link>
+                <Link href="/rotas/123/Daniel">
+                    <li>Daniel</li>
+                </Link>
+            </ul>
         </div>
     )
 }
