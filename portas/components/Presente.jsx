145160751b9a005db30f0564f46349764d5658f3
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-16 17:47:52 -0300

    Update Presente.jsx

--- portas/components/Presente.jsx
+++ portas/components/Presente.jsx
@@ -3,8 +3,10 @@ import styles from '../styles/Presente.module.css'
 export default function Presente() {
     return (
         <div className={styles.presente}>
-            <div className={styles.tampa}>Tampa</div>
-            <div className={styles.corpo}>Corpo</div>
+            <div className={styles.tampa}></div>
+            <div className={styles.corpo}></div>
+            <div className={styles.laco1}></div>
+            <div className={styles.laco2}></div>
         </div>
     )
 }
