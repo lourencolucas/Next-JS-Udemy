a682fd96ddc7b550262800481f0d9cf16f262804
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-17 09:27:48 -0300

    Componente Porta #03
    Aula 78. Componente Porta #03

--- portas/components/Porta.jsx
+++ portas/components/Porta.jsx
@@ -2,9 +2,13 @@ import styles from '../styles/Porta.module.css'
 
 export default function Porta(props) {
     return (
-        <div className={styles.porta}>
-            <div className={styles.numero}>
-                3
+        <div className={styles.area}>
+            <div className={styles.estrutura}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>
+                        3
+                    </div>
+                </div>
             </div>
         </div>
     )
