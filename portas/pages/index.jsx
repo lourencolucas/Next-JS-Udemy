c125f0f59b3d6139d3a857f27e0c79ecbebe5336
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-17 08:15:20 -0300

    Update index.jsx
    75. Box Sizing

--- portas/pages/index.jsx
+++ portas/pages/index.jsx
@@ -2,8 +2,23 @@ import Presente from "../components/Presente";
 
 export default function Home() {
   return (
-    <div>
-      <Presente/>
+    <div style={{display: "flex"}}>
+      {/* <Presente/> */}
+      <div style={{
+        width: "200px",
+        height: "200px",
+        backgroundColor: "red",
+        border: "20px solid white"
+      }}>
+
+      </div>
+      <div style={{
+        width: "200px",
+        height: "200px",
+        backgroundColor: "purple",
+      }}>
+
+      </div>
     </div>
   )
 }
