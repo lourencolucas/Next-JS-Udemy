6ac560f8b14263caf40c65d567766ec7b6e72475
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-17 10:34:39 -0300

    Create porta.js
    Aula 82. Classe Porta

---
+++ portas/model/porta.js
@@ -0,0 +1,28 @@
+export default class Porta {
+    #numero
+    #temPresente
+    #selecionada
+    #aberta
+
+    constructor(numero, temPresente = false, selecionada = false, aberta = false) {
+        this.#numero = numero
+        this.#temPresente = temPresente
+        this.#selecionada = selecionada
+        this.#aberta = aberta
+    }
+    get numero() {
+        return this.#numero
+    }
+
+    get temPresente() {
+        return this.#temPresente
+    }
+
+    get selecionada() {
+        return this.#selecionada
+    }
+
+    get aberta() {
+        return this.#aberta
+    }
+}
