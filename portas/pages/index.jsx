d341c60e4485309a82787fd3f1233da33fbfa031
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-18 08:38:03 -0300

    Update index.jsx

--- portas/pages/index.jsx
+++ portas/pages/index.jsx
@@ -1,11 +1,13 @@
 import Porta from "../components/Porta";
-import Presente from "../components/Presente";
+import PortaModel from "../model/porta";
 
 export default function Home() {
+  const p1 = new PortaModel(1, false, true)
+  const p2 = new PortaModel(2)
   return (
     <div style={{display: "flex"}}>
-      <Porta selecionada/>
-      <Porta />
+      <Porta porta={p1}/>
+      <Porta porta={p2}/>
     </div>
   )
 }
