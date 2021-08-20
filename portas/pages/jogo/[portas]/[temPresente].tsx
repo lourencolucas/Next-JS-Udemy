0deae788331fc9d491467cfe7c70b6407e8f674c
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-20 10:22:51 -0300

    Usando Router
    Aula 92. Usando Router

--- portas/pages/jogo/[portas]/[temPresente].tsx
+++ portas/pages/jogo/[portas]/[temPresente].tsx
@@ -1,12 +1,21 @@
-import styles from "../styles/Jogo.module.css"
-import { useState } from "react";
-import Porta from "../components/Porta";
-import { atualizarPortas, criarPortas } from "../functions/portas";
+import styles from "../../../styles/Jogo.module.css"
+import { useEffect, useState } from "react";
+import Porta from "../../../components/Porta";
+import { atualizarPortas, criarPortas } from "../../../functions/portas";
 import Link from "next/link"
-
+import { useRouter } from "next/router"
 
 export default function Jogo() {
-    const [portas, setPortas] = useState(criarPortas(3, 2))
+    const router = useRouter()
+    const [portas, setPortas] = useState([])
+
+    // recebendo dados das rotas
+    useEffect(() => {
+        const portas = +router.query.portas
+        const temPresente = +router.query.temPresente
+        setPortas(criarPortas(portas, temPresente))
+    }, [router?.query])
+
 
     function renderizarPortas() {
         return portas.map(porta => {
