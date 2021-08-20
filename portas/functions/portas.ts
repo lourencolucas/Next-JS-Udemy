903c14a9d3c489b4b962ae1635538e51e3f54e3f
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-20 08:26:15 -0300

    Lidando com Array de Portas #02
    Aula Lidando com Array de Portas #02

--- portas/functions/portas.ts
+++ portas/functions/portas.ts
@@ -1,3 +1,4 @@
+import porta from "../model/porta";
 import PortaModel from "../model/porta";
 
 export function criarPortas(qtde: number, selecionada: number): PortaModel[] {
@@ -5,5 +6,17 @@ export function criarPortas(qtde: number, selecionada: number): PortaModel[] {
         const numero = i + 1
         const temPresente = numero === selecionada
         return new PortaModel(numero, temPresente)
+    })
+}
+
+export function atualizarPortas(portas: PortaModel[], portaModificada: PortaModel): PortaModel[] {
+    return portas.map(portaAtual => {
+        const igualAModificada = portaAtual.numero === portaModificada.numero
+
+        if(igualAModificada) {
+            return portaModificada
+        } else {
+            return portaModificada.aberta ? portaAtual : portaAtual.desselecionar()
+        }
     })
 }
