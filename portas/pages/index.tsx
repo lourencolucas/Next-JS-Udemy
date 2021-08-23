4ba62f24d31a02636642477d80f069064077a8fc
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-23 10:00:21 -0300

    Finalizando o Formulário
    Aula 94. Finalizando o Formulário

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,20 +1,30 @@
 import styles from "../styles/Formulario.module.css"
 import Cartao from "../components/Cartao";
 import Link from "next/link"
+import EntradaNumerica from "../components/EntradaNumerica";
+import { useState } from "react";
 
 export default function Formulario() {
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
+
   return (
     <div className={styles.formulario}>
       <div>
         <Cartao bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Cartao>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text="Qtde Portas?" value={qtdePortas} onChange={novaQtde => setQtdePortas(novaQtde)}></EntradaNumerica>
+        </Cartao>
       </div>
       <div>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text="Porta com presente?" value={comPresente} onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)}></EntradaNumerica>
+        </Cartao>
+        {/* Definição da escolha das portas */}
         <Cartao bgcolor="#28a085">
-          <Link href={`/jogo/3/2`}>
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`}>
             <h2 className={styles.link}>Iniciar</h2>
           </Link>
         </Cartao>
