20253da3390af58276db28cbebec54e2e944daaf
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-25 09:36:34 -0300

    Componente Estático #04 - Parte 2
    Aula 109. Componente Estático #04 - Parte 2

--- renderezacao/pages/estatico4.jsx
+++ renderezacao/pages/estatico4.jsx
@@ -1,5 +1,6 @@
-//Consumindo a api interna do react
+//Consumindo a api interna do Next
 export async function getStaticProps() {
+    console.log('[Server] Gerando props para o componente...')
     const resp = await fetch('http://localhost:3000/api/produtos')
     const produtos = await resp.json()
     return {
@@ -10,6 +11,7 @@ export async function getStaticProps() {
 }
 
 export default function Estatico4(props) {
+    console.log('[Client] Renderizando o componente...')
     function renderizarProdutos() {
         return props.produtos.map(produto => {
             return <li key={produto.id}>{produto.nome} por R${produto.preco}</li>
