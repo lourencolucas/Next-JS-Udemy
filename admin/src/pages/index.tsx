84ae1232786fdf013a86531cc40f23c1f4f6b959
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-27 13:31:36 -0300

    Update index.tsx

--- admin/src/pages/index.tsx
+++ admin/src/pages/index.tsx
@@ -2,8 +2,8 @@ import Layout from "../components/template/Layout";
 
 export default function Home() {
   return (
-    <Layout titulo="Página Inicial" subtitulo="Construindo um Template Admin">
-      <h3>Conteúdo!</h3>
+    <Layout titulo="Página Inicial" subtitulo="Construindo um Template Admin com Tailwind CSS">
+      <h3>Conteúdo da página!</h3>
     </Layout>
   )
 }
