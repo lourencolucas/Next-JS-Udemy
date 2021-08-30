d158a23cdfee51fd0e36a33d2752186b21088417
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-30 10:59:54 -0300

    Componente Menu Lateral #3
    Aula 170. Componente Menu Lateral #3

--- admin/src/components/template/MenuLateral.tsx
+++ admin/src/components/template/MenuLateral.tsx
@@ -1,18 +1,25 @@
-import { IconeAjustes, IconeCasa, IconeSino } from "../icons";
+import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 //SideMenu
 export default function MenuLateral() {
     return (
-        <aside>
+        <aside className={"flex flex-col"}>
             <div className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}>
                 <Logo />
             </div>
+            <ul className={"flex-grow"}>
+                <MenuItem url="/" texto="Inicio" icone={IconeCasa} />
+                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+            </ul>
             <ul>
-                <MenuItem url="/" texto="Inicio" icone={IconeCasa}/>
-                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}/>
-                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino}/>
+                <MenuItem 
+                texto="Sair" 
+                icone={IconeSair} 
+                onClick={() => console.log('Logout')} 
+                className={`text-red-600 hover:bg-red-400 hover:text-white`}/>
             </ul>
         </aside>
     )
