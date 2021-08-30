5572d2941feaef61716231e7f625fdc80811d5ea
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-30 10:35:36 -0300

    Componente Logo
    Aula 169. Componente Logo

--- admin/src/components/template/MenuLateral.tsx
+++ admin/src/components/template/MenuLateral.tsx
@@ -1,10 +1,14 @@
 import { IconeAjustes, IconeCasa, IconeSino } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 //SideMenu
 export default function MenuLateral() {
     return (
         <aside>
+            <div className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}>
+                <Logo />
+            </div>
             <ul>
                 <MenuItem url="/" texto="Inicio" icone={IconeCasa}/>
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}/>
