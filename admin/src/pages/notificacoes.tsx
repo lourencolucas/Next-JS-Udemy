81a51382eacdd559b222a1903ed94c48e3eb4171
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-30 10:08:01 -0300

    Componente Menu Lateral #2
    Aula 167. Componente Menu Lateral #2

---
+++ admin/src/pages/notificacoes.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Notificacoes() {
+  return (
+    <Layout titulo="Notificações" subtitulo="Área de Notificações">
+      <h3>Notificações do Sistema</h3>
+    </Layout>
+  )
+}
