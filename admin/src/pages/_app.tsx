5abd44674080a3987a4ebc50645dbbee30ed7753
Lucas Oliveira <lucaslou189@gmail.com>
2021-08-31 09:20:37 -0300

    Context API #1
    Aula 173. Context API #1

--- admin/src/pages/_app.tsx
+++ admin/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import AppContext, { AppProvider } from '../data/context/AppContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  )
 }
 
 export default MyApp
+export const AppConsumer = AppContext.Consumer
--- admin/src/pages/notificacoes.tsx
+++ admin/src/pages/notificacoes.tsx
@@ -1,9 +1,12 @@
 import Layout from "../components/template/Layout";
+import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
+  const ctx = useAppData()
   return (
     <Layout titulo="Notificações" subtitulo="Área de Notificações">
-      <h3>Notificações do Sistema</h3>
+      {/* Com hook e contextAPI */}
+      <h3>{ctx.nome}</h3> 
     </Layout>
   )
 }
