923e1708725ac4cd3f11020d2d639b4baeab5251
Lucas Oliveira <lucaslou189@gmail.com>
2021-09-08 15:10:21 -0300

    Avatar Usuário
    Aula 184. Avatar Usuário

---
+++ admin/src/pages/perfil.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Perfil() {
+  return (
+    <Layout titulo="Perfil" subtitulo="Área de Perfil do usuário">
+      <h1>Conteúdo do Perfil do usuário</h1>
+    </Layout>
+  )
+}
