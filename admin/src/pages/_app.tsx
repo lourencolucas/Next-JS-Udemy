8b46e983e5f9d0b10b1532de5829e950d3bf4529
Lucas Oliveira <lucaslou189@gmail.com>
2021-09-08 09:47:59 -0300

    Iniciando com Autenticação do Firebase
    Aula 182. Iniciando com Autenticação do Firebase

--- admin/src/pages/_app.tsx
+++ admin/src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
-import AppContext, { AppProvider } from '../data/context/AppContext'
+import { AppProvider } from '../data/context/AppContext'
+import { AuthProvider } from '../data/context/AuthContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </AuthProvider>
   )
 }
 
 export default MyApp
-export const AppConsumer = AppContext.Consumer
